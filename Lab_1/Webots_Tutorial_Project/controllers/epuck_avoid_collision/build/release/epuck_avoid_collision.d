16552dea7e97fa8d367562109ae1af7870d94af0
Adam Chehadi <adampchehadi@gmail.com>
2021-12-03 09:22:13 -0700

    HW 2 + start of HW 3

--- Lab_1/Webots_Tutorial_Project/controllers/epuck_avoid_collision/build/release/epuck_avoid_collision.d
+++
@@ -1,6 +0,0 @@
-build/release/epuck_avoid_collision.o: epuck_avoid_collision.c \
- /usr/local/webots/include/controller/c/webots/distance_sensor.h \
- /usr/local/webots/include/controller/c/webots/types.h \
- /usr/local/webots/include/controller/c/webots/motor.h \
- /usr/local/webots/include/controller/c/webots/robot.h \
- /usr/local/webots/include/controller/c/webots/nodes.h
--- Lab_1/Webots_Tutorial_Project/controllers/epuck_go_forward/build/release/epuck_go_forward.d
+++
@@ -1,5 +0,0 @@
-build/release/epuck_go_forward.o: epuck_go_forward.c \
- /usr/local/webots/include/controller/c/webots/robot.h \
- /usr/local/webots/include/controller/c/webots/types.h \
- /usr/local/webots/include/controller/c/webots/nodes.h \
- /usr/local/webots/include/controller/c/webots/motor.h
